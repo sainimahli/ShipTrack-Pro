3603008cbeb884a8544fe819b63a10cea84c39c2
mprashanth634-jpg <mprashanth634@gmail.com>
2026-07-04 00:25:43 +0530

    Implemented JWT security module foundation

---
+++ backend/src/main/java/com/shiptrackpro/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.shiptrackpro.security;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.shiptrackpro.entity.User;
+import com.shiptrackpro.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userRepository.findByEmail(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found: " + username));
+    }
+
+}
