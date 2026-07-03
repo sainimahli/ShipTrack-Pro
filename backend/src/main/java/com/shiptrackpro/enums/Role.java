174bb40a824d27ff9dfd1f4af0c04edf79a64a91
Manoj Kumar <deekondamanojkumar123@gmail.com>
2026-07-03 23:18:23 +0530

    Add authentication foundation

---
+++ backend/src/main/java/com/shiptrackpro/enums/Role.java
@@ -0,0 +1,9 @@
+package com.shiptrackpro.enums;
+
+public enum Role {
+    CUSTOMER,
+    BUSINESS_CLIENT,
+    LOGISTICS_OPERATOR,
+    SUPPORT_AGENT,
+    ADMINISTRATOR
+}
---
+++ backend/src/main/java/com/shiptrackpro/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.shiptrackpro.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
