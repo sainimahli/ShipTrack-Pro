f4a9ffe2b0e4e962925cd0a333bbce6ff45f74c2
vedant020504 <vedantsingh29303@gmail.com>
2026-07-06 17:45:57 +0530

    Add database extensions and initial seed data

---
+++ database/seed/01_seed_roles.sql
@@ -0,0 +1,6 @@
+INSERT INTO roles (role_name, description) VALUES
+('CUSTOMER', 'Individual customer who books and tracks shipments'),
+('BUSINESS_CLIENT', 'Business account holder with company shipment needs'),
+('LOGISTICS_OPERATOR', 'Staff managing warehouse and shipment operations'),
+('SUPPORT_AGENT', 'Customer support staff handling queries and issues'),
+('ADMINISTRATOR', 'Full system access and management');
