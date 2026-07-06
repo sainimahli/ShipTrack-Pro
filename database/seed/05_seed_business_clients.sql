d4b6de622577fa8b48361bb9c82eee8b02f9ffd3
vedant020504 <vedantsingh29303@gmail.com>
2026-07-07 01:24:16 +0530

    Add database schema and seed files

---
+++ database/seed/05_seed_business_clients.sql
@@ -0,0 +1,2 @@
+INSERT INTO business_clients (user_id, company_name, gst_number, business_type, website) VALUES
+(2, 'Rao Enterprises Pvt Ltd', '09ABCDE1234F1Z5', 'Retail', 'https://raoenterprises.example.com');
